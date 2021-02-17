9e25d646ea9dcc3d46dd213560feba1910d5f13f
Pro-Cr4stinate <limitlessgamingid@yahoo.com>
2021-02-18 01:11:09 +0700

    tes ke 2 final fix

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$servername = "172.17.0.3";
+$servername = "172.17.0.2";
 $username = "root";
 $password = "DockerRules";
 $dbname = "Trucorp";
@@ -13,12 +13,14 @@ if ($conn->connect_error) {
 
 $sql = "SELECT * FROM users";
 $result = $conn->query($sql);
-
+$count = 0;
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
+    $count++;
     echo "id: " . $row["id"]. "<br> - Name: " . $row["nama"]. "<br> kantor " . $row["kantor"]. "<br> <br>";
   }
+  echo "jumlah user dalam database adalah : " . $count
 } else {
   echo "no results";
 }
